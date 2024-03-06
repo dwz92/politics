c8bba7b2b32bc6a3dab99b50e72acfd5130a8c71
tengqi <75350626+dwz92@users.noreply.github.com>
2024-03-05 20:44:41 -0500

    scripts changes, model changes

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -13,14 +13,14 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_csv("data/analysis_data/ces2020_cleaned.csv")
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
+    formula = voted_for ~ gender + education,
     data = analysis_data,
-    family = gaussian(),
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
